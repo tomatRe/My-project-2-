20ad41e406d7e50f3416ff1b769dc40ee98d91cc
Angel Rebollo <angelre.rebollo@gmail.com>
2025-02-27 19:43:32 +0100

    Player controller cleanup

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -6,117 +6,113 @@ using System;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-
+    // public vars
     public Camera playerCamera;
     public Animator animator;
     public GameObject SitPosition1;
-    public float walkSpeed = 3f;
-    public float runSpeed = 3f;
-    public float jumpPower = 7f;
     public float gravity = 10f;
-    public float lookSpeed = 2f;
     public float lookXLimit = 100f;
     public float defaultHeight = 2f;
-    public float crouchHeight = 1f;
-    public float crouchSpeed = 3f;
+    [SerializeField] public float walkSpeed = 0.9f;
+    [SerializeField] public float lookSpeed = 2f;
 
-    public float defaultFOV = 60f;
-    public float walkFOV = 61f;
-    public float runFOV = 66f;
-    public float fovLerpSpeed = 5f;
-    public float speed = 5f;
+    [SerializeField] public float defaultFOV = 60f;
+    [SerializeField] public float walkFOV = 61f;
+    [SerializeField] public float runFOV = 66f;
+    [SerializeField] public float fovLerpSpeed = 5f;
+    [SerializeField] public float speed = 5f;
+
+    // private vars
 
     private Vector3 moveDirection = Vector3.zero;
-    private float rotationX = 100;
+    private float cameraRotationX = 100;
     private CharacterController characterController;
-
     private bool canMove = true;
     private bool canLook = true;
     private bool IsSitting = false;
+
+    // player inputs
+    private float mouseX = 0;
+    private float mouseY = 0;
+    private float joystickX = 0;
+    private float joystickY = 0;
 
     void Start()
     {
         animator = GetComponent<Animator>(); // Cambiado a Animator
         characterController = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
+        characterController.height = defaultHeight;
         Cursor.visible = false;
         SitPosition1 = GameObject.Find("SitPosition1");
     }
 
     void Update()
     {
+        GetInputs();
         Move();
+        UpdateAnimations();
+        RotateCamera();
         canSeat();
     }
 
-
-    void Move()
+    void GetInputs()
     {
-        if (canMove) { 
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        Vector3 right = transform.TransformDirection(Vector3.right);
+        joystickX = Input.GetAxis("Horizontal");
+        joystickY = Input.GetAxis("Vertical");
+        mouse x = Input.GetAxis("Mouse X");
+        mouse y = Input.GetAxis("Mouse Y");
+    }
 
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
-        float movementDirectionY = moveDirection.y;
-        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
-
-
-        float moveInput = Input.GetAxis("Vertical"); 
-        bool isWalking = Mathf.Abs(moveInput) > 0.1f; 
-         if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
-        {
-            moveDirection.y = jumpPower;
-        }
-        else
-        {
-            moveDirection.y = movementDirectionY;
-        }
-
-        if (!characterController.isGrounded)
-        {
-            moveDirection.y -= gravity * Time.deltaTime;
-        }
-
-        if (Input.GetKey(KeyCode.R) && canMove)
-        {
-            characterController.height = crouchHeight;
-            walkSpeed = crouchSpeed;
-            runSpeed = crouchSpeed;
-        }
-        else
-        {
-            characterController.height = defaultHeight;
-            walkSpeed = 0.9f;
-            runSpeed = 0.8f;
-        }
-        characterController.Move(moveDirection * Time.deltaTime);
-
-
-        AdjustFOV(curSpeedX, curSpeedY, isRunning);
-
+    void UpdateAnimations()
+    {
+        bool isWalking = Mathf.Abs(joystickY) > 0.1f;
         animator.SetBool("IsWalking", isWalking);
+
         if (isWalking)
         {
             animator.SetBool("IsWalking", true);
-
         }
-        else {
+        else
+        {
             animator.SetBool("IsWalking", false);
         }
-        
     }
-        RotateCamera();
+
+    void Move()
+    {
+        if (canMove) 
+        {
+            // movimiento del jugador x y
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+
+            float curSpeedX = walkSpeed * joystickY;
+            float curSpeedY = walkSpeed * joystickX;
+
+            moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+            
+            // efecto gravedad
+
+            if (!characterController.isGrounded)
+            {
+                moveDirection.y -= gravity * Time.deltaTime;
+            }
+
+            characterController.Move(moveDirection * Time.deltaTime);
+
+            AdjustFOV(curSpeedX, curSpeedY, isRunning);        
+        }
     }
 
     void RotateCamera()
     {
-        rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
-        rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        cameraRotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+        cameraRotationX = Mathf.Clamp(cameraRotationX, -lookXLimit, lookXLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(cameraRotationX, 0, 0);
         transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
     }
+
     void canSeat()
     {
         if (Input.GetKey(KeyCode.E))
@@ -131,14 +127,16 @@ public class PlayerMovement : MonoBehaviour
                     sitChair();
                     SitPosition();                    
                 }
-             } 
-    }
+            } 
+        }
+
         if (Input.GetKey(KeyCode.Q))
         {
             animator.SetBool("IsSitting", false);
             canMove = true;
         }
     }
+
     void SitPosition()
     {
         // Desactiva el CharacterController para mover al jugador directamente
@@ -148,12 +146,12 @@ public class PlayerMovement : MonoBehaviour
         transform.position = SitPosition1.transform.position;
         transform.rotation = SitPosition1.transform.rotation;
     }
+
     void sitChair()
     {
          animator.SetBool("IsSitting", true);
          canMove = false;
     }
-
    
     void AdjustFOV(float speedX, float speedY, bool isRunning)
     {
